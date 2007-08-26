004edc6d9f721ce6a63ea71ba6f44d30aa8ce015
Peter Simons <simons@cryp.to>
2007-08-26 17:16:48 +0200

    Provide word-counting function for buffers.

--- WordCounting.hs
+++ WordCounting.hs
@@ -1,7 +1,9 @@
-module WordCounting where
+module WordCounting ( WordCount(..), initWC, wc, wcBuffer ) where
 
-type Count = Int
-data WordCount = WC !Bool !Count !Count !Count
+import Foreign
+import Data.Char
+
+data WordCount = WC !Bool !Int !Int !Int
                      deriving (Show)
 
 initWC :: WordCount
@@ -13,3 +15,9 @@ wc ' '  (WC _     l w c) = WC True   l    w   (c+1)
 wc '\t' (WC _     l w c) = WC True   l    w   (c+1)
 wc  _   (WC True  l w c) = WC False  l  (w+1) (c+1)
 wc  _   (WC False l w c) = WC False  l    w   (c+1)
+
+wcBuffer :: (Ptr Word8, Int) -> WordCount -> IO WordCount
+wcBuffer (_, 0) st@(WC _ _ _ _)  = return st
+wcBuffer (p, n) st@(WC _ _ _ _)  = do
+  c <- fmap (chr . fromIntegral) (peek p)
+  wcBuffer (p `plusPtr` 1, n - 1) (wc c st)
