260427b1b8e3c5295f30b32947674bacc882102e
Peter Simons <simons@cryp.to>
2007-08-28 03:13:00 +0200

    Major cleanup.

--- new-io.hs
+++ new-io.hs
@@ -12,6 +12,13 @@ import Foreign
 import WordCounting
 import Glue
 
+-- * Memory Buffers
+
+type ByteSize   = Int
+type BytePtr    = Ptr Word8
+type Buffer     = (BytePtr, ByteSize)
+type BufferSize = ByteSize
+
 -- * Stream Processig Arrow
 
 readerSP' :: (Monad m) => m Bool -> m a -> SP m () a -> SP m () a
@@ -20,9 +27,6 @@ readerSP' waiter reader eof = Block $
 
 readerSP :: (Monad m, Monoid a) => m Bool -> m a -> SP m () a
 readerSP waiter reader = readerSP' waiter reader (Put mempty zeroArrow)
-
-slurpSP :: (MonadIO m) => Handle -> MsecTimeout -> SP m () ByteString
-slurpSP h to = readerSP (waitForInput h to) (liftIO (hGetNonBlocking h (8 * 1024)))
 
 writerSP :: (Monad m) => (a -> m ()) -> SP m a ()
 writerSP = mapSP
@@ -45,6 +49,20 @@ byLine = Get (wait empty)
     run' (rest:[])   = Get (wait rest)
     run' (str:rest)  = Put str (run' rest)
 
+-- * SPIO
+
+type SPIO i o = forall m. MonadIO m => SP m i o
+
+slurpSP :: Handle -> MsecTimeout -> SPIO () ByteString
+slurpSP h to = readerSP (waitForInput h to) (liftIO (hGetNonBlocking h (8 * 1024)))
+
+bufferReader :: Handle -> MsecTimeout -> Buffer -> SPIO () Buffer
+bufferReader h to (p,n) =
+    readerSP' (waitForInput h to) (liftM (\i -> (p,i)) (liftIO (hGetBufNonBlocking h p n))) (Put (p,0) zeroArrow)
+
+bufferWriter :: Handle -> MsecTimeout -> SPIO Buffer ()
+bufferWriter h to = mapSP $ \(p,n) -> liftIO (timeoutError (outputTimeout h) to (hPutBuf h p n))
+
 -- * Word-counting Example Code
 
 wcByteString :: (Monad m) => ByteString -> StateT WordCount m ()
@@ -57,18 +75,36 @@ wcStrSP = Get (run initWC)
       | null buf  = Put st zeroArrow
       | otherwise = Get (run $! foldl' (flip wc) st buf)
 
-bufferReader :: (MonadIO m) => Handle -> MsecTimeout -> (Ptr Word8, Int) -> SP m () (Ptr Word8, Int)
-bufferReader h to (p,n) =
-    readerSP' (waitForInput h to) (liftM (\i -> (p,i)) (liftIO (hGetBufNonBlocking h p n))) (Put (p,0) zeroArrow)
-
-wcBufSP :: (MonadIO m) => SP m (Ptr Word8, Int) WordCount
+wcBufSP :: SPIO Buffer WordCount
 wcBufSP = Get (run initWC)
   where
-    run :: (MonadIO m) => WordCount -> (Ptr Word8, Int) -> SP m (Ptr Word8, Int) WordCount
+    run :: (MonadIO m) => WordCount -> Buffer -> SP m Buffer WordCount
     run st@(WC _ _ _ _) (_,0) = Put st zeroArrow
     run st@(WC _ _ _ _) (p,n) = Block $ liftM (Get . run) (liftIO (wcBuffer (p,n) st))
 
 -- * HTTP Daemon
+
+httpd :: BufferSize -> (Handle,MsecTimeout) -> (Handle,MsecTimeout) -> IO ()
+httpd bufsize (hIn,toIn) (hOut,toOut) = do
+  modifyIOError (\e -> annotateIOError e "httpd" Nothing Nothing) $
+    withoutBuffering hIn $
+      withoutBuffering hOut $
+        allocaBytes bufsize $ \ptr ->
+          runErrorT $
+            runSP $
+              let reader        = bufferReader hIn toIn (ptr,bufsize)
+                  writer        = bufferWriter hOut toOut
+                  throwShutdown = mapSP (\buf -> if nullbuf buf then throwError "" else return buf)
+                  nullbuf (p,n) = p == nullPtr || n == 0
+              in
+              reader >>> httpdSP >>> throwShutdown >>> writer
+  return ()
+
+data HttpEvent = Request ByteString Int Int
+               deriving (Show)
+
+httpdSP :: SPIO Buffer Buffer
+httpdSP = mapSP (\_ -> return (nullPtr,0))
 
 -- * Command-line driver
 
@@ -81,7 +117,7 @@ main = do
   hSetBuffering stdout (BlockBuffering (Just bufsize))
   hSetBuffering stderr LineBuffering
 
-  let consume :: (Ptr Word8, Int) -> StateT WordCount IO ()
+  let consume :: Buffer -> StateT WordCount IO ()
       consume buf = get >>= \st -> liftIO (wcBuffer buf st) >>= put
 
   allocaBytes bufsize $ \ptr -> do
