d34d40663ab4edb483ff7c5be3f69f82bf779705
Peter Simons <simons@cryp.to>
2007-09-08 15:22:36 +0200

    Improved speed by accessing the ByteString internals.

--- cat-bytestring.hs
+++ cat-bytestring.hs
@@ -1,16 +1,24 @@
 module Main ( main ) where
 
 import System.IO
+import qualified Data.ByteString.Base as Str
 import qualified Data.ByteString as Str
+import Data.ByteString ( ByteString )
 
 bufsize :: Int
 bufsize = 4 * 1024
 
+hGet :: Handle -> ByteString -> IO ByteString
+hGet h buf = do i <- Str.unsafeUseAsCStringLen buf (\(p,n) -> hGetBuf h p n)
+                return (Str.unsafeTake i buf)
+
 catString :: Handle -> Handle -> IO ()
-catString hIn hOut = Str.hGet hIn bufsize >>= loop
+catString hIn hOut = Str.create bufsize (\_ -> return ()) >>= input
   where
-  loop buf | Str.null buf = return ()
-           | otherwise    = Str.hPut hOut buf >> catString hIn hOut
+  input buf      = hGet hIn buf >>= output buf
+  output buf b
+    | Str.null b = return ()
+    | otherwise  = Str.hPut hOut b >> input buf
 
 main :: IO ()
 main = do
