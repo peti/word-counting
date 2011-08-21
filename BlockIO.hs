6ddfaa6c27f9dd1443fd2c8b6715f269f41b1d93
Peter Simons <simons@cryp.to>
2011-08-21 18:39:52 +0200

    BlockIO: follow advice from hlint

--- BlockIO.hs
+++ BlockIO.hs
@@ -1,12 +1,11 @@
-{-# OPTIONS -fglasgow-exts #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 {- |
    Module      :  BlockIO
-   Copyright   :  (c) 2005-02-10 by Peter Simons
-   License     :  GPL2
+   License     :  BSD3
 
    Maintainer  :  simons@cryp.to
    Stability   :  provisional
-   Portability :  GHC 6.x upwards
+   Portability :  DeriveDataTypeable
 
    'runLoop' drives a 'BlockHandler' with data read from the
    input stream until 'hIsEOF' ensues. Everything else has
@@ -45,15 +44,15 @@ withBuffer 0 = fail "BlockIO.withBuffer with size 0 doesn't make sense"
 withBuffer n = bracket cons dest
   where
   cons = mallocArray (fromIntegral n) >>= \p -> return (Buf n p 0)
-  dest = \(Buf _ p _) -> free p
+  dest (Buf _ p _) = free p
 
 -- |Drop the first @n <= size@ octets from the buffer.
 
 flush :: ByteCount -> Buffer -> IO Buffer
 flush 0 buf               = return buf
 flush n (Buf cap ptr len) = assert (n <= len) $ do
-  let ptr' = ptr `plusPtr` (fromIntegral n)
-      len' = (fromIntegral len) - (fromIntegral n)
+  let ptr' = ptr `plusPtr` fromIntegral n
+      len' = fromIntegral len - fromIntegral n
   when (len' > 0) (copyArray ptr ptr' len')
   return (Buf cap ptr (fromIntegral len'))
 
@@ -71,11 +70,11 @@ slurp to h b@(Buf cap ptr len) = do
   timeout to (handleEOF wrap) >>=
     maybe (throw (ReadTimeout to h b)) return
   where
-  wrap = do let ptr' = ptr `plusPtr` (fromIntegral len)
+  wrap = do let ptr' = ptr `plusPtr` fromIntegral len
                 n    = cap - len
             rc <- hGetBufNonBlocking h ptr' (fromIntegral n)
             if rc > 0
-               then return (Buf cap ptr (len + (fromIntegral rc)))
+               then return (Buf cap ptr (len + fromIntegral rc))
                else hWaitForInput h (-1) >> wrap
 
 -- * BlockHandler and I\/O Driver
@@ -99,10 +98,10 @@ runLoopNB
   -> BlockHandler st            -- ^ callback
   -> st                         -- ^ initial callback state
   -> IO st                      -- ^ return final callback state
-runLoopNB mkTO errH hIn cap f initST = withBuffer cap (flip ioloop initST)
+runLoopNB mkTO errH hIn cap f initST = withBuffer cap (`ioloop` initST)
   where
   ioloop buf st = buf `seq` st `seq`
-    handle ((flip errH) st) $ do
+    handle (`errH` st) $ do
       rc <- slurp (mkTO st) hIn buf
       case rc of
         Nothing   -> return st
@@ -123,7 +122,7 @@ type StreamHandler st = [Word8] -> st -> IO (ByteCount, st)
 
 handleStream :: StreamHandler st -> BlockHandler st
 handleStream f buf@(Buf _ ptr len) st = do
-  (i, st') <- peekArray (fromIntegral len) ptr >>= (flip f) st
+  (i, st') <- peekArray (fromIntegral len) ptr >>= flip f st
   buf' <- flush i buf
   return (buf', st')
 
@@ -169,7 +168,7 @@ strstr tok = strstr' 0
 
 splitList :: Eq a => [a] -> [a] -> [[a]]
 splitList d' l' =
-  unfoldr (\x -> if (null x) then Nothing else Just $ nextToken d' [] (snd $ splitAt (length d') x)) (d'++l')
+  unfoldr (\x -> if null x then Nothing else Just $ nextToken d' [] (snd $ splitAt (length d') x)) (d'++l')
   where nextToken _ r [] = (r, [])
-        nextToken d r l@(h:t) | (d `isPrefixOf` l) = (r, l)
+        nextToken d r l@(h:t) | d `isPrefixOf` l = (r, l)
                               | otherwise = nextToken d (r++[h]) t
